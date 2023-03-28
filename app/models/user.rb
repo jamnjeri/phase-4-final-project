860cf7154688e9df644f1d864224dfae967e0fd7
zaiky09 <zamilmozamil@outlook.com>
2023-03-28 20:43:46 +0300

    set up user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+    
+    has_secure_password
+    has_many :recipes
+    has_many :reviews
+
+    validates :username, {
+        length: { minimum: 5, maximum: 10}
+        uniqueness: true
+        presence: true
+    }
+
+    validates :email, {
+        uniqueness: true,
+        presence: true
+    }
+
 end
