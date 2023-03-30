a0751a14401b0f020c6c93163c890990c263d898
zaiky09 <zamilmozamil@outlook.com>
2023-03-30 08:37:18 +0300

    Adding validation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,35 @@
 class User < ApplicationRecord
     
-    has_secure_password
     has_many :recipes
     has_many :reviews
 
+    # Helper method to encrypt password
+    has_secure_password
+
+    # Validations
+    validates :name, {
+        presence: true
+    }
+
     validates :username, {
-        length: { minimum: 5, maximum: 10}
+        length: { minimum: 5, maximum: 20}
         uniqueness: true
         presence: true
     }
 
     validates :email, {
         uniqueness: true,
-        presence: true
+        presence: true,
+        format: { with: URI::MailTo::Email_REGEXP } # for valid email format
     }
 
     validates :password, {
-        length: { minimum: 5, maximum: 15}
-        
+        length: { minimum: 8},
+        if: -> {new_record? || !password.nil?}
+    }
+
+    validates :gender, {
+        presence: true
     }
 
 end
