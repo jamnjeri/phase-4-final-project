088be1b94504cab7cfb9851c6aec35bc0bad63aa
Jamila <jamnjeri@gmail.com>
2023-04-03 08:19:35 +0300

    Updated the application controller to handle authorization and accept cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+ 
+        render json: { errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+    end
+
+    # def user
+    #     user = User.find_by(id: session[:user_id])
+    # end
+
 end
